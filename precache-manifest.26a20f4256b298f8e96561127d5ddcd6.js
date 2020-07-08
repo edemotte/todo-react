127dfeda24b645ea9bc5a494a8b575f76b3e8df1
edemotte <eugenedelain@gmail.com>
2020-07-08 21:24:26 +0530

    Updates

---
+++ precache-manifest.26a20f4256b298f8e96561127d5ddcd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dddbc97a5ccd34ba4a0484a796c837d7",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "71bb769fccbbdb871f10",
+    "url": "/todo-react/static/css/main.f6294cde.chunk.css"
+  },
+  {
+    "revision": "dae8d11cf2f723c9e68f",
+    "url": "/todo-react/static/js/2.2a740fde.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.2a740fde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71bb769fccbbdb871f10",
+    "url": "/todo-react/static/js/main.e9dd4791.chunk.js"
+  },
+  {
+    "revision": "f3cd99faa00badf51f54",
+    "url": "/todo-react/static/js/runtime-main.44c68fc1.js"
+  }
+]);
