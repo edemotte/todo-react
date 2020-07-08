10b2a1d99294eea5059c4b433770c950eba0959b
edemotte <eugenedelain@gmail.com>
2020-07-08 21:01:56 +0530

    todo bare bones

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Todo from "./Todo";
+import "./App.css";
 
 function App() {
+  //we need a list of todo's
+  //useState
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+  console.log(input);
+  console.log(todos);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([input, ...todos]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>To-do App</h1>
+
+      <form>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+        />
+        <button disabled={!input} type="submit" onClick={handleSubmit}>
+          Add Todo
+        </button>
+      </form>
+
+      {todos.map((todo) => (
+        <Todo title={todo} />
+      ))}
     </div>
   );
 }
